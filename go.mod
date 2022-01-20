49856fbc6d83fa9c1ee7e853960ad7c97c77f3dc
leogent <leogent@gmail.com>
2022-01-21 01:33:54 +0700

    Add model for user, product and order

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module simple-ecommerce
 
 go 1.16
 
-require github.com/joho/godotenv v1.4.0
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.4.0
+)
---
+++ model/order.go
@@ -0,0 +1,16 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+type Order struct {
+	gorm.Model
+	User		User	`gorm:"foreignkey:UserId"`
+	Product		Product	`gorm:"foreignkey:ProductID"`
+	UserID		uint
+	ProductID	uint
+	Quantity	int		`json:"quantity"`
+}
+
+func (Order) TableName() string {
+	return "orders"
+}
