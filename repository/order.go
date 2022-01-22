1cfbe5cd502f64f46d68c46576284e609bb9a66a
leogent <leogent@gmail.com>
2022-01-22 14:07:39 +0700

    Add order repository

---
+++ repository/order.go
@@ -0,0 +1,30 @@
+package repository
+
+import (
+	"simple-ecommerce/model"
+
+	"github.com/jinzhu/gorm"
+)
+
+type OrderRepository interface {
+	OrderProduct(int, int, int) error
+}
+
+type orderRepository struct {
+	connection *gorm.DB
+}
+
+func NewOrderRepository() OrderRepository {
+	return &orderRepository{
+		connection: DB(),
+	}
+}
+
+
+func (db *orderRepository) OrderProduct(userID int, productID int, quantity int) error {
+	return db.connection.Create(&model.Order{
+		ProductID: uint(productID),
+		UserID: uint(userID),
+		Quantity: quantity
+	}).Error
+}
